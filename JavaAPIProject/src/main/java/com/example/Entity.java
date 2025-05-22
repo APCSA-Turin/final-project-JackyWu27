f8d0a1621f575e9df597122328a58cc902798dee
JackyWu27 <jackyw27@nycstudents.net>
2025-05-22 06:43:44 -0400

    plz

--- JavaAPIProject/src/main/java/com/example/Entity.java
+++ JavaAPIProject/src/main/java/com/example/Entity.java
@@ -1,5 +1,11 @@
 package com.example;
 
+import java.awt.image.BufferedImage;
+
 public class Entity {
     public int x, y, speed;
+
+    public BufferedImage up1, up2, down1, down2, right1, right2, left1, left2;
+    public String direction;
+
 }
--- JavaAPIProject/src/main/java/com/example/Player.java
+++ JavaAPIProject/src/main/java/com/example/Player.java
@@ -1,7 +1,9 @@
 package com.example;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
 
 public class Player extends Entity{
     GamePanel gp;
@@ -11,8 +13,25 @@ public class Player extends Entity{
         gp = g;
         con = c;
         setDefault();
+        direction = "down";
+        playerImage();
     }
 
+    public void playerImage () {
+        try {
+            up1 = ImageIO.read(getClass().getResourceAsStream("/PlayerArt/Back1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/PlayerArt/Back2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/PlayerArt/Front1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/PlayerArt/Front2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/PlayerArt/Left1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/PlayerArt/Left2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/PlayerArt/Right1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/PlayerArt/Right2.png"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
     public void setDefault () {
         x = 100;
         y = 100;
@@ -21,18 +40,37 @@ public class Player extends Entity{
 
     public void update () {
         if (con.W) {
+            direction = "up";
             y -= speed;
         } else if (con.S) {
+            direction = "down";
             y += speed;
         } else if (con.A) {
+            direction = "left";
             x -= speed;
         } else if (con.D) {
+            direction = "right";
             x += speed;
         }
     }
 
     public void draw (Graphics2D g2) {
-        g2.setColor(Color.white);
-        g2.fillRect(x,y,gp.displayTile,gp.displayTile);
+        BufferedImage image = null;
+        switch (direction) {
+            case "up":
+                image = up1;
+                break;
+        
+            case "down":
+                image = down1;
+                break;
+            case "left":
+                image = left1;
+                break;
+            case "right":
+                image = right1;
+                break;
+        }
+        g2.drawImage(image, x, y, gp.displayTile, gp.displayTile, null);
     }
 }
